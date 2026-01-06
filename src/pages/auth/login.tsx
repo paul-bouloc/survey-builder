b046f2b8b8adc0b647a14a2df08e66e81cec2b0e
Paul Bouloc <37270137+paul-bouloc@users.noreply.github.com>
2026-01-07 00:35:35 +0100

    feat: add page title for login using internationalization support

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,12 +1,21 @@
 import AuthLayout from '@/components/layouts/auth-layout/auth.layout'
 import { AuthForm } from '@/features/auth/components/auth-form.component'
 import type { NextPageWithLayout } from '@/pages/_app'
+import { useTranslations } from 'next-intl'
+import Head from 'next/head'
 
 const LoginPage: NextPageWithLayout = () => {
+  const t = useTranslations('routes.auth')
+
   return (
-    <div className="flex w-full flex-col items-center p-4">
-      <AuthForm />
-    </div>
+    <>
+      <Head>
+        <title>{t('login')}</title>
+      </Head>
+      <div className="flex w-full flex-col items-center p-4">
+        <AuthForm />
+      </div>
+    </>
   )
 }
 
