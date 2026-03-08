f1c31d7b4216a18fc307e5c4fed8b459faba5782
Paul Bouloc <37270137+paul-bouloc@users.noreply.github.com>
2026-03-08 23:12:20 +0100

    feat: add Skeleton component and enhance SurveyEditorHeader with survey data and navigation

--- src/features/surveys/editor/components/survey-editor-header.component.tsx
+++ src/features/surveys/editor/components/survey-editor-header.component.tsx
@@ -1,5 +1,11 @@
 import { Button } from '@/components/ui/button'
+import { Separator } from '@/components/ui/separator'
+import { routes } from '@/config/routes'
+import { selectIsDirty, selectSurvey } from '@/features/surveys/editor/state'
 import { cn } from '@/lib/utils'
+import { useAppSelector } from '@/store/hooks'
+import { ArrowLeftIcon } from 'lucide-react'
+import Link from 'next/link'
 
 interface SurveyEditHeaderComponentProps {
   className?: string
@@ -8,14 +14,29 @@ interface SurveyEditHeaderComponentProps {
 export default function SurveyEditorHeaderComponent({
   className
 }: SurveyEditHeaderComponentProps) {
+  const survey = useAppSelector(selectSurvey)
+  const isDirty = useAppSelector(selectIsDirty)
+
+  if (!survey) return null
+
   return (
     <div
       className={cn(
-        'bg-background relative z-20 flex w-full items-center justify-between border-b p-4 pt-0',
+        'bg-background relative z-20 flex w-full items-center justify-between border-t border-b py-2 pr-4 pl-2',
         className
       )}
     >
-      SurveyEditHeaderComponent <Button disabled>Enregistrer</Button>
+      <div className="flex items-center gap-2">
+        <Button variant="ghost" size="icon" asChild>
+          <Link href={routes.survey.overview.getHref(survey.shortId)}>
+            <ArrowLeftIcon className="size-4" />
+          </Link>
+        </Button>
+        <Separator orientation="vertical" />
+        <h1 className="ml-2 max-w-72 truncate font-semibold">{survey.title}</h1>
+      </div>
+      {isDirty ? '' : ''}
+      <Button disabled>Enregistrer</Button>
     </div>
   )
 }
